f58d989c8c5b50f8f7fcf0598cb5322027ee0c42
fayaz <faizvf@gmail.com>
2016-03-17 23:05:32 -0400

    sampling changes

--- proj1.py
+++ proj1.py
@@ -12,17 +12,129 @@ import random
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+attr = {
+		0: {'name': 'age' , 'type':'continuous'}, 
+		1: {'name': 'class_worker' , 'type':'discrete'}, 
+		2: {'name': 'ind_Code' , 'type':'discrete'}, 
+		3: {'name': 'occ_code' , 'type':'discrete'}, 
+		4: {'name': 'edu' , 'type':'discrete'}, 
+		5: {'name': 'wage' , 'type':'continuous'}, 
+		6: {'name': 'enroll_edu' , 'type':'discrete'}, 
+		7: {'name': 'marital_stat' , 'type':'discrete'}, 
+		8: {'name': 'maj_ind_code' , 'type':'discrete'}, 
+		9: {'name': 'maj_occ_code' , 'type':'discrete'}, 
+		10: {'name': 'race' , 'type':'discrete'}, 
+		11: {'name': 'hispanic' , 'type':'discrete'}, 
+		12: {'name': 'sex' , 'type':'discrete'}, 
+		13: {'name': 'labor_un' , 'type':'discrete'}, 
+		14: {'name': 'unemployment' , 'type':'discrete'}, 
+		15: {'name': 'full_part_time' , 'type':'discrete'}, 
+		16: {'name': 'cap_gain' , 'type':'continuous'}, 
+		17: {'name': 'cap_loss' , 'type':'continuous'}, 
+		18: {'name': 'dividend' , 'type':'continuous'}, 
+		19: {'name': 'tax_filer' , 'type':'discrete'}, 
+		20: {'name': 'reg_prev_res' , 'type':'discrete'}, 
+		21: {'name': 'state_prev_res' , 'type':'discrete'}, 
+		22: {'name': 'house_family_Stat' , 'type':'discrete'}, 
+		23: {'name': 'household_summ' , 'type':'discrete'}, 
+		24: {'name': 'instance_wt' , 'type':'continuous'}, 
+		25: {'name': 'mig_msa' , 'type':'discrete'}, 
+		26: {'name': 'mig_reg' , 'type':'discrete'}, 
+		27: {'name': 'mig_within_reg' , 'type':'discrete'}, 
+		28: {'name': 'live_1year' , 'type':'discrete'}, 
+		29: {'name': 'mig_prev_sun' , 'type':'discrete'}, 
+		30: {'name': 'num_per_worked' , 'type':'continuous'}, 
+		31: {'name': 'fam_un_18' , 'type':'discrete'}, 
+		32: {'name': 'country_father' , 'type':'discrete'}, 
+		33: {'name': 'country_mother' , 'type':'discrete'}, 
+		34: {'name': 'country_self' , 'type':'discrete'}, 
+		35: {'name': 'citizenship' , 'type':'discrete'}, 
+		36: {'name': 'own_self_emp' , 'type':'discrete'}, 
+		37: {'name': 'questionnaire' , 'type':'discrete'}, 
+		38: {'name': 'veteran_ben' , 'type':'discrete'}, 
+		39: {'name': 'weeks_worked' , 'type':'continuous'}, 
+		40: {'name': 'year' , 'type':'discrete'}, 
+		41: {'name': 'income_class' , 'type':'discrete'} 
+	
+	}
+
+def bfs(network):
+	traversal = []
+	q = []
+	visited = [False for i in range(network.shape[0])]
+	for i in range(network.shape[1]):
+		if sum(network[:,i]) == 0:
+			visited[i] = True
+			traversal.append(i)
+			q.insert(0, i)
+	while len(q) > 0:
+		vertex = q.pop()
+		neighbours = np.where(network[vertex, :] == 1)[0]
+		for v in neighbours:
+			if not visited[v]:
+				q.insert(0, v) 
+				traversal.append(v)
+				visited[v] = True
+
+	return traversal
+		
+
 class Sample:
 	def __init__(self, p):
-		pass
+		self.p = p
+		self.network = p.network
+		self.indNodes = self.getIndependentNodes()
 
-class Graph:
-	def __init__(self, adjacencyMatrix, data):
-		self.adjM = adjacencyMatrix
-		self.data = data
+	def getIndependentNodes(self):
+		ind = []
+		for i in self.network.shape[1]:
+			if sum(self.network[:,i]) == 0:
+				ind.append(i)
+		return ind
+		
+	def selectSampleValue(self, cpd):
+		prob = random.uniform(0.0, 1.0)
+		j = 0
+		s = cpd[0][1]
 
-	def generateTabularCPD(self):
-		pass
+		while (prob > s):
+			j += 1
+			s += cpd[j][1]
+
+		return cpd[j][0]
+
+	def getKey(self, node, sample):
+		parents = np.where(self.network[:, node] == 1)[0] 
+		discreteParents = [i for i in parents if attr[i]['type'] == 'discrete']
+		continuousParents = [i for i in parents if attr[i]['type'] == 'continuous']
+
+		discreteParents.sort()
+		if len(continuousParents) == 0:
+			key = [sample[i] for i in discreteParents]
+			return (tuple(key), None)
+		
+		elif len(discreteParents) == 0:
+			return ('continuous', np.array([sample[i] for i in continuousParents]))
+		else:
+			key = [sample[i] for i in discreteParents]
+			return (tuple(key), np.array([sample[i] for i in continuousParents]))
+
+
+	def getSample(self):
+		traversal = bfs(self.network)
+		sample = [-1 for i in range(self.p.shape[1])]
+		for i in self.indNodes:
+			traversal.remove(i)
+			cpd = p.cpd[i]['independent'].getFullProbTable()
+			sample[i] = self.selectSampleValue(cpd)
+
+		while sample.count(-1) > 0:
+			node = traversal.pop(0)
+			key, X = self.getKey(node, sample)
+			cpd = p.cpd[node][key].getFullProbTable(X)
+			sample[i] = self.selectSampleValue(cpd)
+
+
 
 class SpecialDiscreteChild:
 	def __init__(self, classLabel):
@@ -41,7 +153,7 @@ class CPD:
 		self.classLabels = list(set(y))
 		self.classLabels.sort()
 
-		if self.parentType == 'hybrid' or self.parentType == 'continous':
+		if self.parentType == 'hybrid' or self.parentType == 'continuous':
 			if len(set(y)) > 1:
 				clf = LogisticRegression(multi_class='multinomial', solver='newton-cg')
 				clf.fit(X, y)
@@ -58,7 +170,7 @@ class CPD:
 
 
     def getProb(self, X, c):
-        if self.parentType == 'hybrid' or self.parentType == 'continous':
+        if self.parentType == 'hybrid' or self.parentType == 'continuous':
 			clfResults = self.obj.predict_proba(X)
 			res = []
 			for clfResult in clfResults:
@@ -70,66 +182,34 @@ class CPD:
         else:
             return self.obj[c]
 
-       
+	def getFullProbTable(self, X=None):
+		if self.parentType == 'hybrid' or self.parentType == 'continuous':
+			clfResults = self.obj.predict_proba(X)
+			res = []
+			for clfResult in clfResults:
+				if c in self.classLabels:
+					res.append(clfResult[self.classLabels.index(c)])
+				else:
+					res.append(0.0)
+			return zip(self.classLabels, res)
+		else:
+			res = self.obj.items()
+			res.sort(key = lambda x: x[0])
+			return res
+
+      
 		
 
 class Preprocess:
-    def __init__(self, dataFile, N=199523):
+    def __init__(self, dataFile, N=199523, cols = 42):
         self.dataFile = dataFile
         self.rawData = []
         self.cpd = {}
-        self.attr = {
-                0: {'name': 'age' , 'type':'continous'}, 
-                1: {'name': 'class_worker' , 'type':'discrete'}, 
-                2: {'name': 'ind_Code' , 'type':'discrete'}, 
-                3: {'name': 'occ_code' , 'type':'discrete'}, 
-                4: {'name': 'edu' , 'type':'discrete'}, 
-                5: {'name': 'wage' , 'type':'continous'}, 
-                6: {'name': 'enroll_edu' , 'type':'discrete'}, 
-                7: {'name': 'marital_stat' , 'type':'discrete'}, 
-                8: {'name': 'maj_ind_code' , 'type':'discrete'}, 
-                9: {'name': 'maj_occ_code' , 'type':'discrete'}, 
-                10: {'name': 'race' , 'type':'discrete'}, 
-                11: {'name': 'hispanic' , 'type':'discrete'}, 
-                12: {'name': 'sex' , 'type':'discrete'}, 
-                13: {'name': 'labor_un' , 'type':'discrete'}, 
-                14: {'name': 'unemployment' , 'type':'discrete'}, 
-                15: {'name': 'full_part_time' , 'type':'discrete'}, 
-                16: {'name': 'cap_gain' , 'type':'continous'}, 
-                17: {'name': 'cap_loss' , 'type':'continous'}, 
-                18: {'name': 'dividend' , 'type':'continous'}, 
-                19: {'name': 'tax_filer' , 'type':'discrete'}, 
-                20: {'name': 'reg_prev_res' , 'type':'discrete'}, 
-                21: {'name': 'state_prev_res' , 'type':'discrete'}, 
-                22: {'name': 'house_family_Stat' , 'type':'discrete'}, 
-                23: {'name': 'household_summ' , 'type':'discrete'}, 
-                24: {'name': 'instance_wt' , 'type':'continous'}, 
-                25: {'name': 'mig_msa' , 'type':'discrete'}, 
-                26: {'name': 'mig_reg' , 'type':'discrete'}, 
-                27: {'name': 'mig_within_reg' , 'type':'discrete'}, 
-                28: {'name': 'live_1year' , 'type':'discrete'}, 
-                29: {'name': 'mig_prev_sun' , 'type':'discrete'}, 
-                30: {'name': 'num_per_worked' , 'type':'continous'}, 
-                31: {'name': 'fam_un_18' , 'type':'discrete'}, 
-                32: {'name': 'country_father' , 'type':'discrete'}, 
-                33: {'name': 'country_mother' , 'type':'discrete'}, 
-                34: {'name': 'country_self' , 'type':'discrete'}, 
-                35: {'name': 'citizenship' , 'type':'discrete'}, 
-                36: {'name': 'own_self_emp' , 'type':'discrete'}, 
-                37: {'name': 'questionnaire' , 'type':'discrete'}, 
-                38: {'name': 'veteran_ben' , 'type':'discrete'}, 
-                39: {'name': 'weeks_worked' , 'type':'continous'}, 
-                40: {'name': 'year' , 'type':'discrete'}, 
-                41: {'name': 'income_class' , 'type':'discrete'} 
-            
-            }
 
         f = open(self.dataFile)
-        lines = f.readlines()
+        lines = f.readlines()[:N]
         f.close()
 
-        with open(self.dataFile) as data_file:
-            lines = [next(data_file) for x in xrange(N)]
 
         self.numAttrs = len(lines[0].split(','))
         self.map = [defaultdict(lambda: 0) for attributeIndex in range(self.numAttrs)]
@@ -180,13 +260,13 @@ class Preprocess:
 
         return res
 
-    # continous parent to discrete child
+    # continuous parent to discrete child
     def _generateDiscreteChildCPD(self, child, parents):
-		discreteParents = [i for i in parents if self.attr[i]['type'] == 'discrete']
-		continousParents = [i for i in parents if self.attr[i]['type'] == 'continous']
+		discreteParents = [i for i in parents if attr[i]['type'] == 'discrete']
+		continuousParents = [i for i in parents if attr[i]['type'] == 'continuous']
 			   
 		discreteParents.sort()
-		continousParents.sort()
+		continuousParents.sort()
 
 		self.cpd[child] = defaultdict(lambda: 0.0)
 		childDomain = set(self.data[:, child]) 
@@ -200,20 +280,24 @@ class Preprocess:
 			   
 				# select rows with column values matching each of the combination for parent variables
 				y = self.data[np.logical_and.reduce([self.data[:, k] == cols[k] for k in cols])][:, child]
-				if len(continousParents) > 0:
-					X = self.data[np.logical_and.reduce([self.data[:, k] == cols[k] for k in cols])][:, continousParents]
+				if len(continuousParents) > 0:
+					X = self.data[np.logical_and.reduce([self.data[:, k] == cols[k] for k in cols])][:, continuousParents]
 					self.cpd[child][tuple(i)] = CPD('hybrid', X, y)
 				else:
 					self.cpd[child][tuple(i)] = CPD('discrete', None, y)
 
-		else:
-			X = self.data[:, continousParents]
+		elif len(continuousParents) > 0:
+			X = self.data[:, continuousParents]
 			y = self.data[:, child]
-			self.cpd[child]['continous'] = CPD('continous', X, y)
+			self.cpd[child]['continuous'] = CPD('continuous', X, y)
+
+		else:
+			y = self.data[:, child]
+			self.cpd[child]['independent'] = CPD('independent', None, y)
 
     def _generateCCCPD(self, child, parents):
-        continousParents = [i for i in parents]
-        X = self.data[:, continousParents]   
+        continuousParents = [i for i in parents]
+        X = self.data[:, continuousParents]   
         y = self.data[:, child]
 
         xp = np.linspace(-5, 50, 100)
